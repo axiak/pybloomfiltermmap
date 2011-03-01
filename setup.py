d3f781915cf4120a2cf1601925908f31ab002f1b
Michael Axiak <mike@axiak.net>
2011-02-28 22:26:41 -0500

    Version bump to 0.2.22

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ ext_modules = [Extension("pybloomfilter",
 
 setup(
   name = 'pybloomfiltermmap',
-  version = "0.1.20",
+  version = "0.1.22",
   author = "Michael Axiak",
   author_email = "mike@axiak.net",
   url = "http://github.com/axiak/pybloomfiltermmap/",
--- src/pybloomfilter.pyx
+++ src/pybloomfilter.pyx
@@ -1,4 +1,4 @@
-VERSION = (0, 1, 20)
+VERSION = (0, 1, 22)
 AUTHOR = "Michael Axiak"
 
 
